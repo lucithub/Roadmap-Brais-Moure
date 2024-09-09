48e527d11b4c8293013f8046d4901b5827142a7f
lucithub <lucasvelez2013@gmail.com>
2024-09-09 02:05:59 -0300

    Ejercicio 00 - C#

---
+++ #00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//1
+// URL del sitio web oficial de C# : https://docs.microsoft.com/en-us/dotnet/csharp/
+//2
+// Comentario de una línea
+/*
+ * Comentario de varias líneas
+ */
+//3
+var miVariable = "Hola Mundo";
+const string miConstante = "Hola Mundo";
+//4
+string miString = "Hola Mundo";
+int miInt = 1;
+bool miBool = true;
+float miFloat = 1.0f;
+double miDouble = 1.0;
+char miChar = 'a';
+byte miByte = 1;
+short miShort = 1;
+long miLong = 1;
+decimal miDecimal = 1.0m;
+//5
+Console.WriteLine("Hola, C#!");
